fbd6f5ae04297eda46f6ec083381c08443418668
58160639 <58160639@go.buu.ac.th>
2017-10-25 22:45:09 +0700

    not past

--- BookLookup.spec.js
+++ BookLookup.spec.js
@@ -1,10 +1,40 @@
+function BookSearch(isbnService) {
+    this.isbnService = isbnService
+
+    this.search = (name, cover,isbn) => {
+        var web = this.isbnService(name, cover,isbn)
+        return {
+            name: web.name,
+            cover: web.cover,
+            isbn: '978-1117891-234'
+        }
+    }
+}
 
 test('AmazonSearch', ()=> {
     //Arrange
-    const AmazonSearch = jest.fn()
+    const mockAmazonSearch = jest.fn()
         .mockReturnValue({
             isbn:'978-1117891-234',
             title:'JavaScript Good Ports',
             image:'/cover/xyzji.jpg'
         })
-    })
+    var app = new BookSearch(mockAmazonSearch)
+
+    //Act
+    var name = 'JavaScript Good Ports'
+    var cover = '/cover/xyzji.jpg'
+    var isbn = '978-1117891-234'
+    var result = app.search(name, cover,isbn)
+
+    //Assert
+    expect(mockAmazonSearch).toHaveBeenCalled()
+    expect(mockAmazonSearch).toHaveBeenCalledWith(name, cover,isbn)
+    expect(result).toHaveProperty('name')
+    expect(result).toHaveProperty('cover')
+    expect(result).toHaveProperty('isbn')
+    expect(result.title).toEqual('JavaScript Good Ports')
+    expect(result.image).toEqual('/cover/xyzji.jpg')
+    expect(result.isbn).toEqual('978-1117891-234')
+
+})
